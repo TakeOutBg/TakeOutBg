f42b0255e025686af4044538c85994b77794bd09
DEVILKXH <devilkxh@gamil.com>
2018-04-07 23:12:02 +0800

    修改

--- demo-service-impl/src/main/java/com/demo/service/impl/UserServiceImpl.java
+++ demo-service-impl/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.demo.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.demo.entity.User;
 import com.demo.inner.base.serviceimpl.BaseServiceImpl;
 import com.demo.mapper.UserMapper;
 import com.demo.service.UserService;
 
+@Service
 public class UserServiceImpl extends BaseServiceImpl<User, UserMapper> implements UserService{
 
 	
